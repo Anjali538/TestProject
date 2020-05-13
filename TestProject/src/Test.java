04b5e25ee658ed707cf8dec2436fbc9ab27c5932
anjsingh2 <anjsingh2@WKWIN5342144.global.publicisgroupe.net>
2020-05-13 18:23:44 +0530

    updated Test.java

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Learning Git with the Eclipse");
+		System.out.println("Learning Git with the Eclipse change");
 
 	}
 
